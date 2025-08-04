ba83047a99c44a7bb9afab2c97c23dc27c247026
Shivkumarrathod <shivarathod244@gmail.com>
2025-08-04 17:25:51 +0530

    user registration

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,6 @@
-const userRegistration =async (req, res) => {
+import User from '../models/userModel.js';
+
+const userRegistration = async (req, res) => {
   try {
     const { username, email, password } = req.body;
 
@@ -8,22 +10,28 @@ const userRegistration =async (req, res) => {
       return res.status(400).json({ message: 'User already exists' });
     }
 
-
     // Create new user
     const newUser = new User({
       username,
       email,
-      password: hashedPassword,
+      password,
     });
 
     await newUser.save();
-    res.status(201).json({ message: 'User registered successfully' });
+
+    // Remove password before sending response
+    const userResponse = newUser.toObject();
+    delete userResponse.password;
+
+    res.status(201).json({ 
+      message: 'User registered successfully', 
+      user: userResponse 
+    });
 
   } catch (err) {
+    console.log(err);
     res.status(500).json({ message: 'Server error', error: err.message });
   }
 };
 
-export { 
-    userRegistration 
-};
+export { userRegistration };
