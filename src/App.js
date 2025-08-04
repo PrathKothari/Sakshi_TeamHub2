bb9742ae6eb0a8d5a73fa668416743e14f8c4d4d
sakshi <sakshisrivatsa04@gmail.com>
2025-08-05 00:02:21 +0530

    frontend changes

--- src/App.js
+++ src/App.js
@@ -5,30 +5,25 @@ import TeamDetails from './pages/TeamDetails';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import './App.css';
+import { Link } from 'react-router-dom';
 
-function Home() {
+function LandingPage() {
   return (
     <div className="home" style={{ textAlign: 'center', paddingTop: '3rem' }}>
       <h1>Welcome to TeamHub</h1>
-
-      {/* Banner Image */}
-      <img
-        src="/teamhub_banner.png"
-        alt="TeamHub Banner"
-        className="home-banner"
-      />
-
-      {/* Signup Link */}
+      <img src="/teamhub_banner.png" alt="TeamHub Banner" className="home-banner" />
       <Link to="/signup" className="signup-link">Sign Up</Link>
     </div>
   );
 }
+
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
+          <Route path="/" element={<LandingPage />} />
           <Route path="/login" element={<Login />} />
           <Route path="/" element={<Home />} />
           <Route path="/team/:id" element={<TeamDetails />} />
